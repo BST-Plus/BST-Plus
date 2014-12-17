9665499e65fdfdfdccf215c56da5abda0ab124e2
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-17 17:31:45 +0000

    Update index.php

--- index.php
+++ index.php
@@ -11,22 +11,22 @@
             <h2><a href="<?php the_permalink(); ?>"><?php the_title()?></a></h2>
             <h4>
               <em>
-                <span class="text-muted" class="author"><?php _e('By', 'bst'); echo " "; the_author() ?>,</span>
+                <span class="text-muted" class="author"><?php _e('By', 'bst-plus'); echo " "; the_author() ?>,</span>
                 <time  class="text-muted" datetime="<?php the_time('d-m-Y')?>"><?php the_time('jS F Y') ?></time>
               </em>
             </h4>
           </header>
           <?php the_post_thumbnail(); ?>
-          <?php the_content( __( '&hellip; ' . __('Continue reading', 'bst' ) . ' <i class="glyphicon glyphicon-arrow-right"></i>', 'bst' ) ); ?>
+          <?php the_content( __( '&hellip; ' . __('Continue reading', 'bst-plus' ) . ' <i class="glyphicon glyphicon-arrow-right"></i>', 'bst-plus' ) ); ?>
           <p class="text-muted" style="margin-bottom: 20px;">
-            <i class="glyphicon glyphicon-folder-open"></i>&nbsp; <?php _e('Filed under', 'bst'); ?>: <?php the_category(', ') ?><br/>
-            <i class="glyphicon glyphicon-comment"></i>&nbsp; <?php _e('Comments', 'bst'); ?>: <?php comments_popup_link(__('None', 'bst'), '1', '%'); ?>
+            <i class="glyphicon glyphicon-folder-open"></i>&nbsp; <?php _e('Filed under', 'bst-plus'); ?>: <?php the_category(', ') ?><br/>
+            <i class="glyphicon glyphicon-comment"></i>&nbsp; <?php _e('Comments', 'bst-plus'); ?>: <?php comments_popup_link(__('None', 'bst-plus'), '1', '%'); ?>
           </p>
           <hr/>
         </article>
         <?php endwhile; ?>
         <ul class="pagination">
-          <li class="older"><?php next_posts_link('&laquo; ' . __('Older', 'bst')) ?></li>
+          <li class="older"><?php next_posts_link('&laquo; ' . __('Older', 'bst-plus')) ?></li>
           <li class="newer"><?php previous_posts_link(__('Newer', 'bst') . ' &raquo;') ?></li>
         </ul>
         <?php else: ?>
