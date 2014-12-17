e12340e39317027a65197f953c8eddf89081f923
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-17 17:23:50 +0000

    Update enqueues.php

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -3,18 +3,16 @@
 /*
 OPTIONAL: Google CDN jQuery with a local fallback
 See http://www.wpcoke.com/load-jquery-from-cdn-with-local-fallback-for-wordpress/
-If you want to use this, simply delete (or comment-out the block of code below (lines 8-26) 
+If you want to use this, simply delete (or comment-out) the block of code below lines 9-27 
 */
 
 if( !is_admin()){
-//	$url = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'; 
-	$url = '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'; 
+	$url = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'; 
 	$test_url = @fopen($url,'r'); 
 	if($test_url !== false) { 
 		function load_external_jQuery() {
 			wp_deregister_script('jquery'); 
-//			wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'); 
-			wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'); 
+			wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'); 
 			wp_enqueue_script('jquery'); 
 		}
 		add_action('wp_enqueue_scripts', 'load_external_jQuery'); 
@@ -28,17 +26,10 @@ if( !is_admin()){
 	}
 }
 
-/*
-OPTIONAL:
-If you have problems with using the Google CDN hosted jQuery:
--- Comment-out line 11 and un-comment line 10 
--- Comment-out line 17 and un-comment line 16 
-*/
-
-function bst_enqueues() {
+function bst_plus_enqueues() {
 /*
 OPTIONAL: Enqueue WordPress's onboard jQuery
-Simply delete the next lines of code below (line 35-36) if you don't want to use WordPress's onboard jQuery
+Simply un-comment the next lines of code below (line 35-36) if you want to use WordPress's onboard jQuery
 */
 //	wp_register_script('jquery', get_bloginfo('template_url').'/js/jquery-1.11.1.min.js', __FILE__, false, '1.11.1', true);
 //	wp_enqueue_script( 'jquery' );
@@ -48,10 +39,13 @@ Simply delete the next lines of code below (line 35-36) if you don't want to use
 
 /*
 OPTIONAL: Bootstrap Theme enqueued.
-Simply delete the next two lines of code below (lines 45-46) if you don't want the Bootstrap Theme.
+Simply delete (or comment-out) the next two lines of code below (lines 45-46) if you don't want the Bootstrap Theme.
 */
-  	wp_register_style('bst-css', get_template_directory_uri() . '/css/bst.css', false, null);
-	wp_enqueue_style('bst-css');
+  	wp_register_style('bootstrap-theme-css', get_template_directory_uri() . '/css/bootstrap-theme.min.css', false, null);
+	wp_enqueue_style('bootstrap-theme-css');
+
+  	wp_register_style('bst-plus-css', get_template_directory_uri() . '/css/bst-plus.css', false, null);
+	wp_enqueue_style('bst-plus-css');
 
   	wp_register_script('modernizr', get_template_directory_uri() . '/js/modernizr-2.6.2.min.js', false, null, true);
 	wp_enqueue_script('modernizr');
@@ -65,13 +59,13 @@ Simply delete the next two lines of code below (lines 45-46) if you don't want t
   	wp_register_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.js', false, null, true);
 	wp_enqueue_script('bootstrap-js');
 
-	wp_register_script('bst-js', get_template_directory_uri() . '/js/bst.js', false, null, true);
-	wp_enqueue_script('bst-js');
+	wp_register_script('bst-plus-js', get_template_directory_uri() . '/js/bst-plus.js', false, null, true);
+	wp_enqueue_script('bst-plus-js');
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}
 }
-add_action('wp_enqueue_scripts', 'bst_enqueues', 100);
+add_action('wp_enqueue_scripts', 'bst_plus_enqueues', 100);
 
 ?>
