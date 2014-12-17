5fd88003865168421f83c6c55967a27c4fdbd8ed
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-17 17:28:10 +0000

    Update widgets.php

--- functions/widgets.php
+++ functions/widgets.php
@@ -6,9 +6,9 @@ function bst_widgets_init() {
     Sidebar (one widget area)
      */
     register_sidebar( array(
-        'name' => __( 'Sidebar', 'bst' ),
+        'name' => __( 'Sidebar', 'bst-plus' ),
         'id' => 'sidebar-widget-area',
-        'description' => __( 'The sidebar widget area', 'bst' ),
+        'description' => __( 'The sidebar widget area', 'bst-plus' ),
         'before_widget' => '<section class="%1$s %2$s">',
         'after_widget' => '</section>',
         'before_title' => '<h3>',
@@ -19,9 +19,9 @@ function bst_widgets_init() {
     Footer (three widget areas)
      */
     register_sidebar( array(
-        'name' => __( 'Footer', 'bst' ),
+        'name' => __( 'Footer', 'bst-plus' ),
         'id' => 'footer-widget-area',
-        'description' => __( 'The footer widget area', 'bst' ),
+        'description' => __( 'The footer widget area', 'bst-plus' ),
         'before_widget' => '<div class="%1$s %2$s col-sm-4">',
         'after_widget' => '</div>',
         'before_title' => '<h3>',
@@ -29,6 +29,6 @@ function bst_widgets_init() {
     ) );
 
 }
-add_action( 'widgets_init', 'bst_widgets_init' );
+add_action( 'widgets_init', 'bst_plus_widgets_init' );
 
 ?>
