1ea68f942608f9bf39c228a80414da8cac5c9a4f
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-30 12:56:33 +0000

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -15,32 +15,22 @@
 	You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.
 </div>
 <![endif]-->
-  
-<div class="container">
-  <div class="row">
-    <div class="col-sm-12">
-      <h1 id="site-title">
-      	<a class="text-muted" href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a>
-      </h1>
-    </div>
-  </div>
-</div>
 
-<div class="container">
-    <nav class="navbar navbar-default navbar-static" role="navigation">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <?php /* <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> */ ?>
-      </div><!-- /.navbar-header -->
-      <div class="collapse navbar-collapse" id="navbar">    
-        <?php				
+<nav class="navbar navbar-default navbar-static-top navbar-upper">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-upper">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <?php /* <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> */ ?>
+    </div>
+    <div class="collapse navbar-collapse" id="navbar-upper">
+      <?php				
             $args = array(
-              'theme_location' 	=> 'navbar-left',
+              'theme_location' 	=> 'navbar-upper-left',
               'depth' 		=> 0,
               'container'	=> false,
               'fallback_cb' 	=> false,
@@ -52,7 +42,54 @@
         <?php get_template_part('includes/navbar-search'); ?>
         <?php				
             $args = array(
-              'theme_location' 	=> 'navbar-right',
+              'theme_location' 	=> 'navbar-upper-right',
+              'depth' 		=> 0,
+              'container'	=> false,
+              'fallback_cb' 	=> false,
+              'menu_class' 	=> 'nav navbar-nav navbar-right',
+              'walker' 		=> new BootstrapNavMenuWalker()
+            );
+            wp_nav_menu($args);
+        ?>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      <h1 id="site-title">
+      	<a class="text-muted" href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a>
+      </h1>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+    <nav class="navbar navbar-default navbar-static navbar-lower" role="navigation">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-lower">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <?php /* <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> */ ?>
+      </div><!-- /.navbar-header -->
+      <div class="collapse navbar-collapse" id="navbar-lower">    
+        <?php				
+            $args = array(
+              'theme_location' 	=> 'navbar-lower-left',
+              'depth' 		=> 0,
+              'container'	=> false,
+              'fallback_cb' 	=> false,
+              'menu_class' 	=> 'nav navbar-nav',
+              'walker' 		=> new BootstrapNavMenuWalker()
+            );
+            wp_nav_menu($args);
+        ?>
+        <?php				
+            $args = array(
+              'theme_location' 	=> 'navbar-lower-right',
               'depth' 		=> 0,
               'container'	=> false,
               'fallback_cb' 	=> false,
