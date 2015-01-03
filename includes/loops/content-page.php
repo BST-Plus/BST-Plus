2b78d842ca078bec53bf158cf6c9989b3684565a
Themebly <cmwwebfx@gmail.com>
2015-01-03 20:11:47 +1030

    Added hooks to content page loop

--- includes/loops/content-page.php
+++ includes/loops/content-page.php
@@ -4,15 +4,20 @@ The Page Loop
 =============
 */
 ?>
-
+<?php tha_content_before(); ?>
+<?php tha_content_top(); ?>
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
+	<?php tha_entry_before(); ?>
     <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
         <header>
+        	<?php tha_entry_top(); ?>
             <h2><?php the_title()?></h2>
         </header>
         <?php the_content()?>
     </article>
+    <?php tha_entry_bottom(); ?>
 <?php endwhile; else: ?>
+<?php tha_entry_after(); ?>
 <?php wp_redirect(get_bloginfo('siteurl').'/404', 404); ?>
 <?php exit; ?>
 <?php endif; ?>
