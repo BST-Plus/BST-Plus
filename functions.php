b8a51b39f4c18d6ab953b7031a1d5c08ed6c478e
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-07 13:00:57 +0000

    Update functions.php
    Removed closing "?>"

--- functions.php
+++ functions.php
@@ -18,5 +18,3 @@ add_action('after_setup_theme', 'true_load_theme_textdomain');
 function true_load_theme_textdomain(){
     load_theme_textdomain( 'bst-plus', get_template_directory() . '/languages' );
 }
-
-?>
