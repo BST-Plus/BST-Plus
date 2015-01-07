3397b0e34e5d7ba782a5aaf3b89312f92854367d
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-07 13:46:52 +0000

    Update navbar-search.php

--- includes/navbar-search.php
+++ includes/navbar-search.php
@@ -2,6 +2,7 @@
 /*
 Navbar search form
 ==================
+
 If you don't want a search form in your navbar, then delete the link to this PHP page from within the navbar in header.php.
 Then you can insert the Search Widget into the sidebar.
 */
