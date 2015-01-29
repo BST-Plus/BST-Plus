cbb29144dd53f5d84fb4b1e22446a904a2af93a2
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-29 11:31:12 +0000

    Update includes.php

--- functions/includes.php
+++ functions/includes.php
@@ -1,40 +1,19 @@
 <?php
 
 /*
-These action hooks add the theme header(s), sidebar(s) and footer(s) to the 
-*fallback* (ancient) WordPress core header, sidebar and footer.
+Action hooks add the theme header(s), sidebar(s) and footer(s) to the 
+WordPress fallback header, sidebar and footer (found in 
+wordpress/wp-content/includes/.
 
-These fallbacks are used by WordPress because the header(s), sidebar(s) 
-and footer(s) of this theme are not in the root folder but in 'includes/'.
-
-Note: There is some accompanying jQuery that removes these defaults from 
+There is some accompanying jQuery that removes these defaults from 
 the HTML (see js/bst-plus.js) - so all that is displayed is your 
 header(s), sidebar(s) and footer(s).
 
 Note: you can't use these IDs in your theme design, because the jQuery
 mentioned above removes them: #page, #header, #sidebar, #footer.
 
-These action hooks below are used so that you can use the *normal* 
-get_header(), get_sidebar() and get_footer calls in your templates 
-(index.php, etc.). Therefore:
-
-(1.) Any plugin that also makes use of get_header() etc. will work exactly 
-as it should.
-
-(2.) You can create custom parts (e.g. header-home.php) and call them in
-the normal way (e.g. in index.php, you would have 
-<?php get_header('home'); ?>
-
-(3.) And this theme is tidy and organized, with all the template parts in 
-the includes/ folder.
-
-If you don't want to use these action hooks, then simply remove the line
-require_once locate_template('functions/includes.php') from functions.php
-and edit all your base templates (index.php etc.) so that they have:
-
-<?php get_template_part('includes/header.php') ?>
-<?php get_template_part('includes/sidebar.php') ?>
-<?php get_template_part('includes/footer.php') ?>
+More information can be found in the BST-Plus wiki:
+https://github.com/BST-Plus/BST-Plus/wiki
 */
 
 function bst_plus_header( $name = null ) {
