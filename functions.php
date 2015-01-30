26df310cf52c05c095a721bc9ade414c1443505b
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-30 15:14:22 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@ All the functions are in the PHP pages in the functions/ folder.
 require_once locate_template('/functions/cleanup.php');
 require_once locate_template('/functions/setup.php');
 require_once locate_template('/functions/enqueues.php');
-require_once locate_template('/functions/includes.php');
 require_once locate_template('/functions/navbar.php');
 require_once locate_template('/functions/widgets.php');
 require_once locate_template('/functions/search.php');
