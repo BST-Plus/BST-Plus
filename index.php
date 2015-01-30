798b8e1148b067dfc4fa955ca7b992176ff8e065
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-30 15:17:28 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<?php get_header(); ?>
+<?php get_template_part('includes/header'); ?>
 
 <div class="container">
-  <div class="row">
+	<div class="row">
     
-    <div class="col-xs-12 col-sm-8">
-      <div id="content" role="main">
+		<div class="col-xs-12 col-sm-8">
+			<div id="content" role="main">
 				<?php get_template_part('includes/loops/content', get_post_format()); ?>
-      </div><!-- /#content -->
-    </div>
+			</div><!-- /#content -->
+		</div>
     
-    <div class="col-xs-6 col-sm-4" id="sidebar" role="navigation">
-       <?php get_sidebar(); ?>
-    </div>
+		<div class="col-xs-6 col-sm-4" id="sidebar" role="navigation">
+			<?php get_template_part('includes/header'); ?>
+		</div>
     
-  </div><!-- /.row -->
+	</div><!-- /.row -->
 </div><!-- /.container -->
 
-<?php get_footer(); ?>
+<?php get_template_part('includes/footer'); ?>
