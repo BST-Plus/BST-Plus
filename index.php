51813bd3d6a1efb7e137191beb6189cb2ba52a5d
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-02-10 07:55:50 +0000

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 		</div>
     
 		<div class="col-xs-6 col-sm-4" id="sidebar" role="navigation">
-			<?php get_template_part('includes/header'); ?>
+			<?php get_template_part('includes/sidebar'); ?>
 		</div>
     
 	</div><!-- /.row -->
